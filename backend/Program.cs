08a8d1c60259f8f8517612ad35efb0d7929eb6b3
Julio Cesar Dias Dos Santos <juliozulu2008@gmail.com>
2025-10-30 13:30:49 -0300

    Seeguindo tutorial do Microsoft

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,21 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUi(
+        options =>
+        {
+            options.DocumentPath = "/openapi/v1.josn";
+        }
+    );
 }
 
 app.UseHttpsRedirection();
